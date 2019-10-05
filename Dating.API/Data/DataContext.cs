99c808c990b6138cdb3d0520b4cb344cde033632
DavidMaximaWang <qunlingwang@gmail.com>
2019-10-05 01:41:27 +0000

    creating register method in auth controller

--- Dating.API/Data/DataContext.cs
+++ Dating.API/Data/DataContext.cs
@@ -7,5 +7,6 @@ namespace Dating.API.Data
     {
         public DataContext(DbContextOptions<DataContext> options):base(options){}
         public DbSet<Value> Values { get; set; }
+        public DbSet<User> User { get; set; }
     }
 }
