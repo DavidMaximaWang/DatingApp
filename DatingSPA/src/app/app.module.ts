b3a6bd98d3aa154918d409be728839a04c462987
DavidMaximaWang <qunlingwang@gmail.com>
2019-10-08 01:32:47 +0000

    add ngx bootstrap to power the login dropdown menu

--- DatingSPA/src/app/app.module.ts
+++ DatingSPA/src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
+import { BsDropdownModule } from 'ngx-bootstrap';
 import {HttpClientModule} from '@angular/common/http';
 import {FormsModule} from '@angular/forms';
+
+import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { AuthService } from './_services/auth.service';
 import { HomeComponent } from './home/home.component';
@@ -20,7 +21,8 @@ import { ErrorInterceptorProvider } from './_services/error.interceptor';
    imports: [
       BrowserModule,
       HttpClientModule,
-      FormsModule
+      FormsModule,
+      BsDropdownModule.forRoot(),
    ],
    providers: [
       AuthService,
